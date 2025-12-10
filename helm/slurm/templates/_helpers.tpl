kincl/slurm-operator

commit ac05f32a5ce44a8e5238b12273d665e9ebd9f877

fix: avoid storage and claim null values
There seems to be a bug with Kubernetes, and/or Helm, where the chart
default/implicit values.yaml will fail when `storageClassName=null`,
despite being an acceptable value. When explicitly providing a
values.yaml with `storageClassName=null`, this works as expected.

$ helm -n slurm install slurm helm/slurm                      # fails
$ helm -n slurm install slurm helm/slurm -f helm/values.yaml  # succeeds

Making existingClaim="" avoid the same null error as storageClassName.
But for storageClassName in particular, null != "", hence we must
specially handle the truthy and falsy with respect to the empty string
and omit when null.

Changelog: Fixed - Avoid `storageClassName=null` error, which occurs when
 using the Slurm chart default `values.yaml`, implicitly used by helm.

helm/slurm/templates/_helpers.tpl
@@ -136,3 +136,18 @@ Ref: https://github.com/helm/helm/issues/11376#issuecomment-1256831105
 {{- end -}}
 {{- mulf (float64 $value) $unit -}}
 {{- end -}}
+
+{{/*
+ToYaml dict after handling storageClassName.
+Ref: https://github.com/helm/helm/issues/2600
+*/}}
+{{- define "toYaml-set-storageClassName" -}}
+{{- $out := . | default dict -}}
+{{ $storageClassName := get $out "storageClassName" }}
+{{- if or $storageClassName (eq $storageClassName "-") (eq $storageClassName "") }}
+  {{- $_ := set $out "storageClassName" $storageClassName -}}
+{{- else }}
+  {{- $out = omit $out "storageClassName" -}}
+{{- end }}
+{{- toYaml $out -}}
+{{- end -}}

helm/slurm/templates/controller/controller-cr.yaml
@@ -89,8 +89,9 @@ spec:
     {{- include "format-container" .Values.controller.logfile | nindent 4 }}
   {{- include "format-podTemplate" $podTemplate | nindent 2 }}
   {{- with .Values.controller.persistence }}
+  {{- $persistence := fromYaml (include "toYaml-set-storageClassName" .) }}
   persistence:
-    {{- toYaml . | nindent 4 }}
+    {{- toYaml $persistence | nindent 4 }}
   {{- end }}{{- /* with .Values.controller.persistence */}}
   {{- with .Values.controller.service }}
   service:

helm/slurm/values.yaml
@@ -179,8 +179,10 @@ controller:
     enabled: true
     # -- Name of the existing `PersistentVolumeClaim` to use instead of creating one.
     # If this is not empty, then certain other fields will be ignored.
-    existingClaim: null
+    existingClaim: ""
     # -- (string) The name of the `StorageClass` for the created `PersistentVolumeClaim`.
+    # If set to "-" or "", it disables dynamic storage provisioning.
+    # If undefined or set to null, your default storage provisioner is used.
     # Ref: https://kubernetes.io/docs/concepts/storage/storage-classes/
     storageClassName: null
     # Create the `PersistentVolumeClaim` with the desired access modes.

commit dffd1c4fc123df35164665ff997da8f91eb5a35d

fix(chart): fix storageClassName handling regression
Regression caused by commit ac05f32a.

helm/slurm/templates/_helpers.tpl
@@ -143,8 +143,10 @@ Ref: https://github.com/helm/helm/issues/2600
 */}}
 {{- define "toYaml-set-storageClassName" -}}
 {{- $out := . | default dict -}}
-{{ $storageClassName := get $out "storageClassName" }}
-{{- if or $storageClassName (eq $storageClassName "-") (eq $storageClassName "") }}
+{{ $storageClassName := dig "storageClassName" nil $out }}
+{{- if or (eq $storageClassName "") (eq $storageClassName "-") -}}
+  {{- $_ := set $out "storageClassName" "" -}}
+{{- else if $storageClassName }}
   {{- $_ := set $out "storageClassName" $storageClassName -}}
 {{- else }}
   {{- $out = omit $out "storageClassName" -}}